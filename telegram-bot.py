b42341299eaab53cb873d3d5a2692bba0690f15c
Asrorbek <149676769+User2012GitHub@users.noreply.github.com>
2024-05-14 20:23:17 +0500

    Update telegram-bot.py
    version 2

--- telegram-bot.py
+++ telegram-bot.py
@@ -8,3 +8,13 @@ cursor.execute('CREATE TABLE IF NOT EXISTS medicine '
 cursor.execute('''INSERT INTO medicine (id, name, bdate, price) 
 VALUES (1, 'Paratsetamol', '1 year', '5000')''')
 cursor.execute('''INSERT INTO  medicine (id, name, bdate, price)
+VALUES (5, 'Anzibel', '8 months', '12000')''')
+cursor.execute('''INSERT INTO  medicine (id, name, bdate, price)
+VALUES (3, 'Trimol', '7 months', '26000')''')
+
+conn.commit()
+
+cursor.execute('''SELECT * FROM medicine''')
+about = input("Dori nomini kiriting: ")
+result = cursor.fetchall()
+print(result)
